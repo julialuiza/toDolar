a9374d928edc148563f91520e516ad7dce46d78f
Julia Luiza <jlslc@icomp.ufam.edu.br>
2021-05-29 23:48:40 -0400

    [feature] add unit tests to function toDolar()

---
+++ toDolar.test.js
@@ -0,0 +1,16 @@
+const toDolar = require('./toDolar');
+
+test('Teste com valor < 0', () => {
+  realValue = -1;
+  expect(toDolar(realValue)).toEqual("realValue can't be less than 0");
+});
+
+test('Teste com valor = 0', () => {
+  realValue = 0;
+  expect(toDolar(realValue)).toEqual(0);
+});
+
+test('Teste com valor > 2', () => {
+  realValue = 2;
+  expect(toDolar(realValue)).toEqual(0.38);
+});
