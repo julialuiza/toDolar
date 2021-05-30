9bd752e1ed0e6c8e336ae0213c33aa83e9c27ece
Julia Luiza <jlslc@icomp.ufam.edu.br>
2021-05-30 15:32:51 -0400

    [hotfix] fix bug in toDolar.js

--- toDolar.js
+++ toDolar.js
@@ -7,14 +7,12 @@ function toDolar(realValue) {
   const valueDolar = 5.23; // Valor coletado em 29/05/2021
 
   if (realValue < 0) {
-    return "realValue can't be less than 0;
+    return "realValue can't be less than 0";
   }
 
   const realDolar = parseFloat((realValue / valueDolar).toFixed(2));
 
   return realDolar;
 }
-
-console.log('2R$ em Dólar:' + toDolar(2));
 
 module.exports = toDolar;
