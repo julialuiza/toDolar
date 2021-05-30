df7dae0bf7fe0113360f6d47260423b5c1fa7554
Julia Luiza <jlslc@icomp.ufam.edu.br>
2021-05-29 23:47:36 -0400

    [feature] add function toDolar()

---
+++ toDolar.js
@@ -0,0 +1,18 @@
+/**
+ * Returns the value Real (R$) to Dolar ($)
+ * @param { double } realValue
+ */
+
+function toDolar(realValue) {
+  const valueDolar = 5.23; // Valor coletado em 29/05/2021
+
+  if (realValue < 0) {
+    return "realValue can't be less than 0";
+  }
+
+  const realDolar = parseFloat((realValue / valueDolar).toFixed(2));
+
+  return realDolar;
+}
+
+module.exports = toDolar;
